afa4c31afc2df80ed74b85164b1c611d22091cfe
David <drh697@gmail.com>
2022-02-20 15:52:39 -0600

    comments

--- src/middleware/ErrorHandler.ts
+++ src/middleware/ErrorHandler.ts
@@ -1,8 +1,20 @@
 import { Middleware, ExpressErrorMiddlewareInterface } from "routing-controllers";
-
+import { Request, Response } from "express";
+/**
+ * ErrorHandler middleware class 
+ * @decorator `Middleware({ type: "after" })`
+ */
 @Middleware({ type: "after" })
 export class ErrorHandler implements ExpressErrorMiddlewareInterface {
-  public error( error: any, request: any, response: any, ext: (err: any) => any) {
+  /**
+   * Error handler method 
+   * @param {any} error
+   * @param {Request} request 
+   * @param {Response} response
+   * @param {any} next
+   * @returns {any}
+   */
+  public error( error: any, request: Request, response: Response, next: (err: any) => any): any {
 
     if (response.headersSent) {
       return;
